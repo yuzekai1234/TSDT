6ca007fade5259155d61abde67eb4327bcad23d8
yourname <youremail@xxx.com>
2024-04-27 16:14:05 +0800

    ppt9测试数据库(1) Before refactoring the functional_tests

--- notes/main.py
+++ notes/main.py
@@ -29,17 +29,20 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        # 他在文本框中输入了“Buy flowers"
-        inputbox.send_keys('Buy flowers')
-
-        # 他按下回车键后页面更新了
-        # 待办事项表格中显示了“1: Buy flowers"
+        # # 他在文本框中输入了“Buy flowers"
+        # inputbox.send_keys('Buy flowers')
+        #
+        # # 他按下回车键后页面更新了
+        # # 待办事项表格中显示了“1: Buy flowers"
+        # inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertIn('1: Buy flowers', [row.text for row in rows])
+        self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
 
         # 页面中又显示了一个文本框，可以输入其他的事项
         # 他输入了“Use peacock feathers to make a fly"
