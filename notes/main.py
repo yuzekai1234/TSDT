3e35d2223a006f71f911b3103f4ebecf7fc20389
yourname <youremail@xxx.com>
2024-04-27 15:06:45 +0800

    ppt8重构(1) Functional test now checks we can input a to-do item

--- notes/main.py
+++ notes/main.py
@@ -1,14 +1,54 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+from selenium.webdriver.common.by import By
+
 class NewVisitorTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.browser=webdriver.Chrome()
-    def tearDown(self) -> None:
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
         self.browser.quit()
-    def test_can_start_a_list_and_retreive_it_later(self):
-        self.browser.get("http://localhost:8000")
-        self.assertIn('To-Do',self.browser.title),"Browser title was "+self.browser.title
-        self.fail("finish the test!")
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 新访客访问网站的首页
+        # 他听说这是一个很酷的待办事项应用
+        self.browser.get('http://localhost:8000')
+
+        # 他注意到网页的标题和头部都包含“To-Do”这个词
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        # 应用邀请他输入一个待办事项
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 他在文本框中输入了“Buy flowers"
+        inputbox.send_keys('Buy flowers')
+
+        # 他按下回车键后页面更新了
+        # 待办事项表格中显示了“1: Buy flowers"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy flowers', [row.text for row in rows])
+
+        # 页面中又显示了一个文本框，可以输入其他的事项
+        # 他输入了“Use peacock feathers to make a fly"
+        # 做事清单很有趣
+        self.fail('Finish the test!')
+
+        # 页面更新了，现在的清单中显示了这两个待办事项
+
+
 
 if __name__ == '__main__':
     unittest.main()
