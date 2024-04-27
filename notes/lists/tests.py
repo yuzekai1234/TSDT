f5b08188482ded468783fdd2446b6e590cbaa635
yourname <youremail@xxx.com>
2024-04-27 15:30:24 +0800

    ppt8重构(2) Refactoring

--- notes/lists/tests.py
+++ notes/lists/tests.py
@@ -4,9 +4,10 @@ from .views import home_page
 from django.http import HttpRequest
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found=resolve('/')
-        self.assertEquals(found.func,home_page)
+
+    def test_uses_home_template(self):
+        response=self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
     def test_home_page_return_correct_html(self):
         request =HttpRequest()
         response=home_page(request)
