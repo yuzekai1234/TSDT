c553dcc4f408d74e6777fa494c19dada112362de
yourname <youremail@xxx.com>
2024-05-23 22:34:48 +0800

    ppt13 手动部署（1） FT can test real_server

--- notes/functional_tests/tests.py
+++ notes/functional_tests/tests.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -10,6 +11,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
+        real_server=os.environ.get('REAL_SERVER')
+        if real_server:
+            self.live_server_url='http://'+real_server
 
     def tearDown(self):
         self.browser.quit()
