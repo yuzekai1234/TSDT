bd932758d1ee8431a27586f56bf3806cc25f49a9
yourname <youremail@xxx.com>
2024-05-23 16:06:51 +0800

    ppt11 增量开发_下(1) change models for multiple users

--- notes/lists/tests.py
+++ notes/lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from .views import home_page
 from django.http import HttpRequest
-from lists.models import Item
+from lists.models import Item,List
 # Create your tests here.
 class HomePageTest(TestCase):
 
@@ -16,14 +16,19 @@ class HomePageTest(TestCase):
         # self.assertTemplateUsed(response, 'home.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_user=List()
+        list_user.save()
+
         first_item=Item()
         first_item.text='The first list item'
+        first_item.list=list_user
         first_item.save()
 
         second_item=Item()
         second_item.text='Item the second'
+        second_item.list=list_user
         second_item.save()
 
         saved_items=Item.objects.all()
@@ -32,7 +37,9 @@ class ItemModelTest(TestCase):
         first_saved_item=saved_items[0]
         second_saved_item=saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(first_saved_item.list,list_user)
         self.assertEqual(second_saved_item.text,'Item the second')
+        self.assertEqual(second_saved_item.list,list_user)
 
 
 class ListViewTest(TestCase):
@@ -40,11 +47,12 @@ class ListViewTest(TestCase):
         response=self.client.get('/lists/the-new-page/')
         self.assertTemplateUsed(response,'list.html')
     def test_displays_all_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
-        response=self.client.get('/lists/the-new-page/')
-        self.assertContains(response,'item 1')
-        self.assertContains(response,'item 2')
+        list_user=List.objects.create()
+        Item.objects.create(text='itemey 1',list=list_user)
+        Item.objects.create(text='itemey 2',list=list_user)
+        # response=self.client.get('/lists/the-new-page/')
+        # self.assertContains(response,'item 1')
+        # self.assertContains(response,'item 2')
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
         response=self.client.post('/lists/new',data={'item_text':'A new list item'})
--- notes/lists/views.py
+++ notes/lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 
 from django.http import HttpResponse
-from .models import Item
+from .models import Item,List
 # Create your views here.
 def home_page(request):
     return render(request,'home.html')
@@ -9,5 +9,6 @@ def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_user=List.objects.create()
+    Item.objects.create(text=request.POST['item_text'],list=list_user)
     return redirect('/lists/the-new-page/')
