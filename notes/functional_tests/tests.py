ef643d553a23a383274d1e2c771256a44bd42c3f
yourname <youremail@xxx.com>
2024-05-23 17:51:21 +0800

    ppt12 布局测试（1） testing for layout + styling

--- notes/functional_tests/tests.py
+++ notes/functional_tests/tests.py
@@ -100,3 +100,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk',page_text)
         # 两个用户都很满意
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:testing')
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x']+inputbox.size['width']/2,
+            512,
+            delta=10
+        )
